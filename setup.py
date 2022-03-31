4b2479bffe257892214ae34d90418a1a4e0ece25
Oleg Smirnov <osmirnov@gmail.com>
2022-03-31 11:40:13 +0200

    scabha2 dep version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,11 @@ import glob
 requirements = ["pyyaml",
                 "nose>=1.3.7",
                 "future-fstrings",
-                "scabha >= 0.6.1", # @ git+https://github.com/caracal-pipeline/scabha2",
+                "scabha >= 0.7.0", 
+                ## OMS: not a fan of this:
+                # @ git+https://github.com/caracal-pipeline/scabha2",
+                ## ...because it interferes with running scabha2 off a dev branch (i.e. if you have a local dev install of scabha,
+                ## pip install stimela will blow it away and replace with master branch...)
                 "ruamel.yaml",
                 "munch",
                 "omegaconf>=2.1pre1",
