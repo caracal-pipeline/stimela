f121ff24c3eda6031f054e32a7102e6bc1917870
Oleg Smirnov <osmirnov@gmail.com>
2022-06-28 14:52:00 +0200

    more packaging fixes

--- setup.py
+++ setup.py
@@ -32,19 +32,23 @@ requirements = [
 PACKAGE_NAME = "stimela"
 __version__ = "2.0rc2"
 
+packages = set(find_packages())
+# these aren't auto-discovered with find_packages() currenty, so add them
+packages.add("stimela_tests")
+packages.add("scabha_tests")
+
 setup(name=PACKAGE_NAME,
       version=__version__,
       description="Radio interferometry workflow management framework",
       author="Sphesihle Makhathini & Oleg Smirnov & RATT",
       author_email="sphemakh@gmail.com",
       url="https://github.com/caracal-pipeline/stimela2",
+      packages=packages,
+      # tell it where to find the xx_tests packages
       package_dir={
-            "stimela": "stimela",
-            "scabha": "scabha",
             "scabha_tests": "tests/scabha_tests",
             "stimela_tests": "tests/stimela_tests"
       },
-      packages=["stimela", "scabha", "stimela_tests", "scabha_tests"],
       include_package_data=True,
       python_requires='>=3.7',
       install_requires=requirements,
