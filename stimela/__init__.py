8f927859e264c717a498e79361440ac3fd972766
sphemakh <sphemakh@gmail.com>
2022-10-25 16:51:07 +0200

    remove used vars

--- stimela/__init__.py
+++ stimela/__init__.py
@@ -1,6 +1,4 @@
 import os
-import re
-import inspect
 from pathlib import Path
 
 __author__ = """Sphesihle Makhathini, Oleg Smirnov and RATT"""
@@ -9,11 +7,8 @@ __version__ = "2.0rc2"
 
 CONFIG = None
 
-# Get to know user
-USER = os.environ["USER"]
 UID = os.getuid()
 GID = os.getgid()
-CAB_USERNAME = re.sub('[^0-9a-zA-Z]+', '_', USER).lower() 
 
 root = os.path.dirname(__file__)
 
@@ -26,31 +21,5 @@ LOG_HOME = os.path.expanduser("~/.stimela")
 Path(LOG_HOME).mkdir(exist_ok=True)
 # This is is the default log file. It logs stimela images, containers and processes
 LOG_FILE = "{0:s}/stimela_logfile.json".format(LOG_HOME)
-
-GLOBALS = {'foo': 'bar'}
-del GLOBALS['foo']
-
-def register_globals():
-    frame = inspect.currentframe().f_back
-    frame.f_globals.update(GLOBALS)
-
-# Get base images
-# All base images must be on dockerhub
-BASE = os.listdir(BASE_PATH)
-CAB = list()
-
-for item in os.listdir(CAB_PATH):
-    itempath = os.path.join(CAB_PATH, item)
-    if os.path.isdir(itempath):
-        try:
-            # These files must exist for a cab image to be valid
-            ls_cabdir = os.listdir(itempath)
-            paramfile = 'parameters.json' in ls_cabdir
-            srcdir = 'src' in ls_cabdir
-        except OSError:
-            continue
-        if paramfile and srcdir:
-            CAB.append(item)
-
 
 from .stimelogging import logger, log_exception
