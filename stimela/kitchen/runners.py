f9fee29b5af53c29c95e9c1ef8f93440002ef73c
sphemakh <sphemakh@gmail.com>
2022-11-17 13:58:24 +0200

    apply step management.environment before runnig step

--- stimela/kitchen/runners.py
+++ stimela/kitchen/runners.py
@@ -1,5 +1,5 @@
 from typing import Dict, Optional, Any
-from stimela import logger, config
+from stimela import config
 
 from .step import Step
 from .batch import Batch
@@ -11,9 +11,17 @@ def run_cab(step: Step, params: Dict[str, Any],
             batch: Batch=None) -> Cab.RuntimeStatus:
     log = step.log
     cab = step.cargo
-    runtime = step.runtime
+
+    ## NOTE(Sphe)
+    # Not sure is this the best implementation
+    # But the runtime environment has to default to config.run unless explicitly given in the step
+    # But more importantly it has be updated by the Cab environment before running the cab
+    
+    step.runtime = step.runtime or subst.config.run.copy()
+    cab.update_environment(subst=subst)
+    step.runtime.env.update(**cab.management.environment)
 
     backend = __import__(f"stimela.backends.{backend.name}", 
                          fromlist=[backend.name])
     
-    return backend.run(cab, runtime=runtime, params=params, log=log, subst=subst, batch=batch, fqname=step.fqname)
+    return backend.run(cab, runtime=step.runtime, params=params, log=log, subst=subst, batch=batch, fqname=step.fqname)
