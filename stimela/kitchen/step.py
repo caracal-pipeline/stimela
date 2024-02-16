7f7fd01dc745f3a07556480203d63fb6e21d68e1
Oleg Smirnov <osmirnov@gmail.com>
2024-02-16 16:01:14 +0200

    fixes #235

--- stimela/kitchen/step.py
+++ stimela/kitchen/step.py
@@ -234,8 +234,8 @@ class Step:
             self.cargo.name = self.cargo.name or self.name
 
             # flatten parameters
-            self.cargo.apply_dynamic_schemas(self.params)
             self.params = self.cargo.flatten_param_dict(OrderedDict(), self.params)
+            self.cargo.apply_dynamic_schemas(self.params)
 
             # if logger is not provided, then init one
             if log is None:
@@ -266,6 +266,7 @@ class Step:
 
     def prevalidate(self, subst: Optional[SubstitutionNS]=None, root=False):
         self.finalize()
+        self.cargo.apply_dynamic_schemas(self.params, subst)
         # validate cab or recipe
         params = self.validated_params = self.cargo.prevalidate(self.params, subst, root=root)
         # add missing outputs
