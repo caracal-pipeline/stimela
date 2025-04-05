fc54d21b840e5e9018b2f78ea594e23039db037a
Oleg Smirnov <osmirnov@gmail.com>
2025-04-05 21:30:52 +0200

    fixes to comments

--- stimela/utils/xrun_asyncio.py
+++ stimela/utils/xrun_asyncio.py
@@ -40,8 +40,8 @@ def dispatch_to_log(log, line, command_name, stream_name, output_wrangler, style
         line, severity = output_wrangler(escape(line), severity)
     else:
         line = escape(line)
-    # escape emojis. Wranglers can return FunkyMessages instead of strings, in which case the 
-    # escaping is aleady done
+    # escape emojis. Check that it's a str -- wranglers can return FunkyMessages instead of strings, in which case the 
+    # escaping is aleady done for us
     if type(line) is str:
         line = re.sub(r":(\w+):", r":[bold][/bold]\1:", line)
     # dispatch to log
